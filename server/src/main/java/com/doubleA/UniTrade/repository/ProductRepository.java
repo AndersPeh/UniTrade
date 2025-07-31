8906930ecb6a5cbd26d34da4ac782acb0b8f2b4d
AndersPeh <pehjyfung@gmail.com>
2025-08-01 03:02:20 +1000

    ProductRepository added

--- server/src/main/java/com/doubleA/UniTrade/repository/ProductRepository.java
+++ server/src/main/java/com/doubleA/UniTrade/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.doubleA.UniTrade.repository;
 
 import com.doubleA.UniTrade.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -18,5 +19,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByBrand(String brand);
 
+// When user types anything, it will be converted to lowercase name. Then it will be compared to lowercase name of
+// products. As long as the user enters name that matches any part of the product name, results will be returned.
+    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))")
     List<Product> findByName(String name);
 }
