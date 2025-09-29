fd5f0d5b382ef00e05cfb8a1f8f7803cc17f5830
AndersPeh <pehjyfung@gmail.com>
2025-09-29 22:36:27 +1000

    Add OrderDto

--- server/src/main/java/com/doubleA/UniTrade/dtos/UserDto.java
+++ server/src/main/java/com/doubleA/UniTrade/dtos/UserDto.java
@@ -13,4 +13,5 @@ public class UserDto {
   private String firstName;
   private String lastName;
   private String email;
+  private List<OrderDto> orders;
 }
--- server/src/main/java/com/doubleA/UniTrade/service/order/IOrderService.java
+++ server/src/main/java/com/doubleA/UniTrade/service/order/IOrderService.java
@@ -1,5 +1,6 @@
 package com.doubleA.UniTrade.service.order;
 
+import com.doubleA.UniTrade.dtos.OrderDto;
 import com.doubleA.UniTrade.model.Order;
 
 import java.util.List;
@@ -8,4 +9,6 @@ public interface IOrderService {
   Order placeOrder(Long userId);
 
   List<Order> getUserOrders(Long userId);
+
+  OrderDto convertToDto(Order order);
 }
