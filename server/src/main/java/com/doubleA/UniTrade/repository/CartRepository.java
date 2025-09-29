8316931182a5504038ee678bef194d5c07eab142
AndersPeh <pehjyfung@gmail.com>
2025-09-29 18:22:44 +1000

    Add CartRepository, CartItemService

---
+++ server/src/main/java/com/doubleA/UniTrade/repository/CartRepository.java
@@ -0,0 +1,6 @@
+package com.doubleA.UniTrade.repository;
+
+import com.doubleA.UniTrade.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {}
