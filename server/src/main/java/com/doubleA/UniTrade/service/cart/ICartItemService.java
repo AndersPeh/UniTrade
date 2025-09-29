0e4ddb07a3ed0c2b5f7eed6165ebf5f93723c251
AndersPeh <pehjyfung@gmail.com>
2025-09-29 18:22:12 +1000

    Add Interfaces for CartItem and CartService

---
+++ server/src/main/java/com/doubleA/UniTrade/service/cart/ICartItemService.java
@@ -0,0 +1,13 @@
+package com.doubleA.UniTrade.service.cart;
+
+import com.doubleA.UniTrade.model.CartItem;
+
+public interface ICartItemService {
+  void addItemToCart(Long cartId, Long productId, int quantity);
+
+  void removeItemFromCart(Long cartId, Long productId);
+
+  void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+  CartItem getCartItem(Long cartId, Long productId);
+}
