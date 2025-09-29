fad72c65bffc463ada8ca16ada8f7dcd3e3d293c
AndersPeh <pehjyfung@gmail.com>
2025-09-29 19:13:24 +1000

    Add CartService

---
+++ server/src/main/java/com/doubleA/UniTrade/service/cart/CartService.java
@@ -0,0 +1,63 @@
+package com.doubleA.UniTrade.service.cart;
+
+import com.doubleA.UniTrade.model.Cart;
+import com.doubleA.UniTrade.model.User;
+import com.doubleA.UniTrade.repository.CartItemRepository;
+import com.doubleA.UniTrade.repository.CartRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CartService implements ICartService {
+  private final CartRepository cartRepository;
+  private final CartItemRepository cartItemRepository;
+
+  @Override
+  public Cart getCart(Long cartId) {
+    return cartRepository
+        .findById(cartId)
+        .orElseThrow(() -> new EntityNotFoundException("Cart Not Found"));
+
+    //    BigDecimal totalAmount = cart.getTotalAmount();
+    //    cart.setTotalAmount(totalAmount);
+    //    return cartRepository.save(cart);
+  }
+
+  @Override
+  public Cart getCartByUserId(Long userId) {
+    return cartRepository
+        .findByUserId(userId)
+        .orElseThrow(() -> new EntityNotFoundException("Cart Not Found"));
+  }
+
+  @Override
+  public void clearCart(Long cartId) {
+    Cart cart = getCart(cartId);
+    cartItemRepository.deleteAllByCartId(cartId);
+    cart.clearCart();
+    cartRepository.deleteById(cartId);
+  }
+
+  @Override
+  public Cart initialiseNewCartForUser(User user) {
+    return Optional.ofNullable(getCartByUserId(user.getId()))
+        // Only create a new cart if there is no cart belongs to the user.
+        .orElseGet(
+            () -> {
+              Cart cart = new Cart();
+              cart.setUser(user);
+              return cartRepository.save(cart);
+            });
+  }
+
+  @Override
+  public BigDecimal getTotalPrice(Long cartId) {
+    Cart cart = getCart(cartId);
+    return cart.getTotalAmount();
+  }
+}
