7e3c182afa6d3116415b893a3f587a79e1e56de6
AndersPeh <pehjyfung@gmail.com>
2025-10-03 17:48:01 +1000

    Add comments for JwtEntryPoint

--- server/src/main/java/com/doubleA/UniTrade/security/jwt/JwtEntryPoint.java
+++ server/src/main/java/com/doubleA/UniTrade/security/jwt/JwtEntryPoint.java
@@ -14,17 +14,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
+
+// For defining what should happen when an unauthenticated user tries to access a secured endpoint
+// without a valid JWT token.
 public class JwtEntryPoint implements AuthenticationEntryPoint {
 
-    @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        final Map<String, Object> body = new HashMap<>();
-        body.put("Error :", "Unauthorized");
-        body.put("Message :", "Invalid credentials");
-        final ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(response.getOutputStream(), body);
-    }
+  @Override
+  // When an AuthenticationException is thrown, this method is called automatically.
+  public void commence(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AuthenticationException authException)
+      throws IOException, ServletException {
+    // Indicate the server is sending back JSON data.
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    // Set HTTP status code to 401 Unauthorized.
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    // Create a custom JSON body containing error message.
+    final Map<String, Object> body = new HashMap<>();
+    body.put("Error :", "Unauthorized");
+    body.put("Message :", "Invalid credentials");
+    final ObjectMapper mapper = new ObjectMapper();
+    // write the JSON object to the HTTP response.
+    mapper.writeValue(response.getOutputStream(), body);
+  }
 }
