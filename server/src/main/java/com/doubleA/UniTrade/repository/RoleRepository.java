a91765fb7dce69e71171b18dd9768f053e1d86a7
AndersPeh <pehjyfung@gmail.com>
2025-10-04 01:59:00 +1000

    Configure UserService and UserRole

---
+++ server/src/main/java/com/doubleA/UniTrade/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.doubleA.UniTrade.repository;
+
+import com.doubleA.UniTrade.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Role findByName(String userRole);
+}
