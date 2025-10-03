ab971025d8d143aa91fdb6a813fa800f2c6c9e65
AndersPeh <pehjyfung@gmail.com>
2025-10-03 18:48:37 +1000

    Add comments for AuthTokenFilter

--- server/src/main/java/com/doubleA/UniTrade/security/jwt/AuthTokenFilter.java
+++ server/src/main/java/com/doubleA/UniTrade/security/jwt/AuthTokenFilter.java
@@ -21,45 +21,54 @@ import java.io.IOException;
 @Component
 public class AuthTokenFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JwtUtils jwtUtils;
-    @Autowired
-    private ShopUserDetailsService userDetailsService;
+  @Autowired private JwtUtils jwtUtils;
 
+  @Autowired private ShopUserDetailsService userDetailsService;
 
-    @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
-        try {
-            String jwt = parseJwt(request);
-            if (StringUtils.hasText(jwt) && jwtUtils.validateToken(jwt)) {
-                String username = jwtUtils.getUsernameFromToken(jwt);
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                var auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(auth);
-            }
-        } catch (Exception e) {
-            sendErrorResponse(response);
-            return;
-        }
-        filterChain.doFilter(request, response);
+  @Override
+  protected void doFilterInternal(
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain)
+      throws ServletException, IOException {
+    try {
+      String jwt = parseJwt(request);
+      // If token exists and is validate:
+      if (StringUtils.hasText(jwt) && jwtUtils.validateToken(jwt)) {
+        String username = jwtUtils.getUsernameFromToken(jwt);
+        // get the email from the token and retrieve user details.
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        // Creates authenticated user object containing user details and roles.
+        var auth =
+            new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        // store the authenticated user object for Spring security to perform authorisation.
+        SecurityContextHolder.getContext().setAuthentication(auth);
+      }
+    } catch (Exception e) {
+      sendErrorResponse(response);
+      return;
     }
+    filterChain.doFilter(request, response);
+  }
 
-    private void sendErrorResponse(HttpServletResponse response) throws IOException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
-        ErrorResponse errorResponse = new ErrorResponse("Invalid access token, please long and try again!");
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonResponse = objectMapper.writeValueAsString(errorResponse);
-        response.getWriter().write(jsonResponse);
-    }
+  private void sendErrorResponse(HttpServletResponse response) throws IOException {
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.setContentType("application/json");
+    ErrorResponse errorResponse =
+        new ErrorResponse("Invalid access token, please long and try again!");
+    ObjectMapper objectMapper = new ObjectMapper();
+    String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+    response.getWriter().write(jsonResponse);
+  }
 
-    public String parseJwt(HttpServletRequest request) {
-        String headerAuth = request.getHeader("Authorization");
-        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
-            return headerAuth.substring(7);
-        }
-        return null;
+  public String parseJwt(HttpServletRequest request) {
+    String headerAuth = request.getHeader("Authorization");
+    if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+      // get the token from the header (starts from index 7 because 0-6 indexes are taken by "Bearer
+      // ".)
+      return headerAuth.substring(7);
     }
+    return null;
+  }
 }
